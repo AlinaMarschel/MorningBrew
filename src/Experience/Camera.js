05cae6e9bb0b590d0c034be1aa903440f29ec050
Alina <alina.marschel@gmail.com>
2022-01-05 13:19:48 +0100

    Camera Animation

--- src/Experience/Camera.js
+++ src/Experience/Camera.js
@@ -15,7 +15,19 @@ export default class Camera
         this.setInstance()
         this.setOrbitControls()
         this.resize()
+        this.setCursor()
         this.update()
+    }
+
+    setCursor() {
+        this.cursor = {}
+        this.cursor.x = 0
+        this.cursor.y = 0
+
+        window.addEventListener('mousemove', (event) => {
+            this.cursor.x = event.clientX / this.sizes.width - 0.5
+            this.cursor.y = event.clientY / this.sizes.height - 0.5
+        })
     }
 
     setInstance()
@@ -45,31 +57,16 @@ export default class Camera
 
     animateCamera()
     {
-            this.scrollY = window.scrollY
+        let parallaxX = this.cursor.x * 0.3
+        let parallaxY = -this.cursor.y * 0.3
 
-            this.cursor = {}
-            this.cursor.x = 0
-            this.cursor.y = 0
-
-            window.addEventListener('mousemove', (event) => {
-                this.cursor.x = event.clientX / this.sizes.width - 0.5
-                this.cursor.y = event.clientY / this.sizes.height - 0.5
-            })
-
-            this.instance.position.y = -this.scrollY / this.sizes.height * 4
-
-            this.parallaxX = this.cursor.x * 0.5
-            this.parallaxY = - this.cursor.y * 0.5
-
-            this.cameraGroup.position.x += (this.parallaxX - this.cameraGroup.position.x) * 5 * this.delta
-            this.cameraGroup.position.y += (this.parallaxY - this.cameraGroup.position.y) * 5 * this.delta
-
-            console.log(this.cursor.x)
+        this.cameraGroup.position.x += (parallaxX - this.cameraGroup.position.x) * 0.01 * this.delta
+        this.cameraGroup.position.y += (parallaxY - this.cameraGroup.position.y) * 0.01 * this.delta
     }
 
     update()
     {
-        this.controls.update()
+        //this.controls.update()
         this.animateCamera()
     }
 }
