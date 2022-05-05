0fad1fc701e774d69dd52e4e281fa2c07fc4af5f
Alina <alina.marschel@gmail.com>
2022-05-05 15:44:02 +0200

    Sprites

--- src/Experience/World/Jungle.js
+++ src/Experience/World/Jungle.js
@@ -17,17 +17,27 @@ export default class Jungle {
 
     setSprite() {
         
-        this.backgroundMaterial = new THREE.SpriteMaterial({ map: this.resources.items.jungleBackground });
+        this.backgroundMaterial = new THREE.SpriteMaterial({ 
+            map: this.resources.items.jungleBackground,
+            fog: false,
+            transparent: false
+        });
+
         this.backgroundSprite = new THREE.Sprite(this.backgroundMaterial);
 
-        this.plant01Material = new THREE.SpriteMaterial({map: this.resources.items.plant01})
+        this.plant01Material = new THREE.SpriteMaterial({
+            map: this.resources.items.plant01,
+        })
+
         this.plant01 = new THREE.Sprite(this.plant01Material)
 
         this.backgroundSprite.scale.set(20, 10 ,0)
         this.backgroundSprite.position.set(0, 0, -10)
+        
 
         this.plant01.scale.set(1, 1, 1)
         this.plant01.position.set(-1.5, -0.5, 2)
+
 
         console.log(this.plant01)
 
